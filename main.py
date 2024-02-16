6251d3dee8b26a0a81bd04ecb5b5e755bfb52de6
moses <mosesberco1@gmail.com>
2024-02-16 18:32:08 +0200

    connecting the db

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
-from fastapi import FastAPI,Depends,HTTPException
+from fastapi import FastAPI,Depends,HTTPException, Body
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,Session
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+from contextlib import asynccontextmanager
 
-DATABASE_URL = "sqlite:///:Users.db:"
-engine_users = create_engine(DATABASE_URL)
+api_app = FastAPI(
+    title="Your FastAPI App",
+    description="Your FastAPI app description",
+    version="0.1",
+    templates="templates"  # Specify the location of the templates directory
+)
+
+
+# Allow all origins for demonstration purposes.
+api_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+DATABASE_URL = "sqlite:///C:/Users/mosheberco/year2/project/New folder/Users.db"
+engine_users = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
 
 Base = declarative_base()
 SessionLocalUsers = sessionmaker(autocommit=False, autoflush=False, bind=engine_users)
-Base.metadata.create_all(bind=engine_users)
 
-def get_db(db_name):
-    if db_name == "Users":
-        db = SessionLocalUsers()
-        try:
-            yield db
-        finally:
-            db.close()
+@asynccontextmanager
+async def lifespan(api_app: FastAPI):
+    Base.metadata.create_all(bind=engine_users)
+    yield
+
+    
+def get_db():
+    def dependcy():
+            db = SessionLocalUsers()
+            try:
+                yield db
+            finally:
+                db.close()
+    return dependcy
 
 
 class User(Base):
@@ -37,8 +66,9 @@ class User(Base):
 def email_in_db(email: str, db: Session):
     return db.query(User).filter(User.Email == email).first() is not None
 
-
-def AddUser(user_data: dict, db: Session = Depends(get_db("Users"))):
+@api_app.post("/add_user")
+async def AddUser(user_data: dict, db: Session = Depends(get_db())):
+    print("addUser activate")
     First_name = user_data.get("First_name")
     Last_name = user_data.get("Last_name")
     Email = user_data.get("Email")
@@ -58,4 +88,13 @@ def AddUser(user_data: dict, db: Session = Depends(get_db("Users"))):
     db.commit()
     return {"message": "User added successfully"}
 
+app = FastAPI(title="main app", lifespan=lifespan)
+html_path = Path(__file__).parent / "index.html"
+app.mount("/api", api_app)
+app.mount("/", StaticFiles(directory="templates",html = True), name="templates")
 
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
