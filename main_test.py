92a34d74d0c90b73f84d0a45e4538330543aa5a9
moses <mosesberco1@gmail.com>
2024-03-05 00:49:00 +0200

    merged the files and adding the connection to the create user file

--- main_test.py
+++ main_test.py
@@ -1,6 +1,9 @@
 import unittest
 import main
+from main import *
 
 class TestMain(unittest.TestCase):
     def test_AddUser(self):
-        self.assertEquals
+        new_person = User(First_name="moshe", Last_name="bercovici", Email="moses@meatmoses.com", Password="123456",
+                    City="beer sheva", Age=25, Proficiency="doctor", Role="admin", Last_login=func.now())
+        self.assertEquals()
